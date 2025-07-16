136cb2b9a988647c207b70dd4759637199d08be3
ralfd <ralfduli24@gmail.com>
2025-07-16 17:51:01 +0100

    Cleanup

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
         home-manager.nixosModules.home-manager 
         {
           users.users.ralfd.isNormalUser = true;
+	  users.users.ralfd.extraGroups = [ "wheel" ]
           #users.users.ralfd.initialPassword = "Cur<>ragh1";
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
